2a0a644906e1bbd9e48a2788c0361493e31081fe
Felipe Diogo <mrholeechit@gmail.com>
2019-03-24 18:26:14 -0300

    first commit :tada:

---
+++ src/main/java/br/com/felipediogo/whitelistapp/WhiteListAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipediogo.whitelistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteListAppApplication.class, args);
+	}
+
+}
